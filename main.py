a3a583e3d96dc71368edf010e8037e6901c23be1
Ronye Sarreira <ronye.m.sarreira@gmail.com>
2024-04-24 15:22:48 +0100

    Teste
    Descricao do Teste

---
+++ main.py
@@ -0,0 +1,7 @@
+from tickets import Tickets
+
+#p1 = Tickets ()
+#print (p1)
+
+p2 = Tickets("233444","por atender","HW2")
+print(p2)
---
+++ tickets.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+class Tickets():
+    def __init__(self, *args):
+        self._data = datetime.now()
+        self._idColab = None
+        self.status = "por atender"
+        self.tipo = "HW"
+
+        if len(args)==1 and isinstance(args[0], Tickets):
+            self.idColab = args[0].idColab
+            self.status = args[0].status
+            self.tipo = args[0].tipo
+        if len(args)==3:
+            self.idColab = args[0]
+            self.status = args[1]
+            self.tipo = args[2]
+
+
+
+
+
+    @property
+    def idColab(self):
+        return self._idColab
+
+
+    @idColab.setter
+    def idColab(self, valor):
+            self._idColab=valor
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, valor):
+        if valor in ("por atender", "atendido"):
+            self._status = valor
+
+    @property
+    def tipo(self):
+        return self._tipo
+
+    @tipo.setter
+    def tipo(self, valor):
+        if valor in ("HW", "SW"):
+            self._tipo = valor
+    def __str__(self):
+
+        return (f"Colaborador: {self.idColab}, status: {self.status}, tipo: {self.tipo}")
